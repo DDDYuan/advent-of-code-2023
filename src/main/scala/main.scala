33ac59742a9cb286f5f8219808fcf2074eb36406
DDDYuan <yuandi93@hotmail.com>
2023-12-03 14:57:58 +0800

    finish day 3

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,8 +1,8 @@
 @main
 def main(): Unit =
-  val result1 = Day2.part1
+  val result1 = Day3.part1
   println("result is:")
   println(result1)
-  val result2 = Day2.part2
+  val result2 = Day3.part2
   println("result is:")
   println(result2)
