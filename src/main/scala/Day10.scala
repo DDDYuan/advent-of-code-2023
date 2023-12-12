07754832b616f02c5292fdbd67c24b776f83bb70
Di Yuan <dyuan@thoughtworks.com>
2023-12-12 15:10:27 +0800

    day 10 part 1

--- src/main/scala/Day10.scala
+++ src/main/scala/Day10.scala
@@ -1,13 +1,48 @@
 object Day10 extends GenericPuzzle("day10.csv"):
-  enum Direction:
+  private enum Direction:
     case LEFT
     case RIGHT
     case UP
     case DOWN
-  private possibleDirection = Map(
-    'S' -> List(Direction.LEFT, Direction.RIGHT, Direction.UP, Direction.DOWN)
-  )
+  private def nextDirectionFrom(current: (Char, Direction)) = current match
+    case ('-', Direction.LEFT)  => Direction.LEFT
+    case ('-', Direction.RIGHT) => Direction.RIGHT
+    case ('|', Direction.UP)    => Direction.UP
+    case ('|', Direction.DOWN)  => Direction.DOWN
+    case ('7', Direction.LEFT)  => Direction.UP
+    case ('7', Direction.DOWN)  => Direction.RIGHT
+    case ('L', Direction.UP)    => Direction.LEFT
+    case ('L', Direction.RIGHT) => Direction.DOWN
+    case ('J', Direction.LEFT)  => Direction.DOWN
+    case ('J', Direction.UP)    => Direction.RIGHT
+    case ('F', Direction.DOWN)  => Direction.LEFT
+    case ('F', Direction.RIGHT) => Direction.UP
+  private def getNextPosition(current: (Int, Int), from: Direction) = from match
+    case Direction.LEFT => (current._1, current._2 + 1)
+    case Direction.RIGHT => (current._1, current._2 - 1)
+    case Direction.UP => (current._1 + 1, current._2)
+    case Direction.DOWN => (current._1 - 1, current._2)
+  private def findStart(map: List[List[Char]]) =
+    val x = map.zipWithIndex.find(_._1.contains('S')).get._2
+    val y = map(x).indexOf('S')
+    (x, y)
+  private def findNext(current: ((Int, Int), Char, Direction), map: List[List[Char]]) =
+    val nextFromDirection = nextDirectionFrom((current._2, current._3))
+    val nextPosition = getNextPosition(current._1, nextFromDirection)
+    val nextChar = map(nextPosition._1)(nextPosition._2)
+    (nextPosition, nextChar, nextFromDirection)
   private def toMap(raw: List[String]) = raw.map(_.toCharArray.toList)
-  override def part1(input:  List[String]): String = ""
-  override def part2(input:  List[String]): String = ""
-
+  private val START_BLOCK = 'L'
+  private val START_FROM = Direction.UP
+  override def part1(input: List[String]): String =
+    val rawMap = toMap(input)
+    val map = rawMap.map(_.map(x => if x == 'S' then START_BLOCK else x))
+    val start = findStart(rawMap)
+    var current = findNext((start, START_BLOCK, START_FROM), map)
+    var path = List(current)
+    while current._1 != start do
+      current = findNext(current, map)
+      path = path.appended(current)
+    println(path)
+    (path.size / 2).toString
+  override def part2(input: List[String]): String = ""
